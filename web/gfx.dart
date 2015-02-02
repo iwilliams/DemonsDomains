3f475bf92c5198276b29bd4d18472dd65ff23b9f
Ian Williams <ianwilliams633@gmail.com>
2015-02-02 14:31:34 -0800

    Initial commit

---
+++ web/gfx.dart
@@ -0,0 +1,170 @@
+import 'package:malison/malison.dart';
+import 'dart:html' as html;
+import 'dart:math';
+
+class Gfx {
+
+  RetroTerminal _terminal;
+  html.CanvasElement _canvas;
+
+  Gfx(int width, int height) {
+
+    _canvas = new html.CanvasElement();
+    html.document.body.children.add(_canvas);
+
+    html.window.onClick.listen((event) => _canvas.requestFullscreen());
+
+
+    // Create a new terminal. CanvasTerminal uses your browser's fonts.
+    // RetroTerminal uses a built in DOS-style Code Page 437 font.
+    _terminal = new RetroTerminal.shortDos(width, height, this._canvas);
+
+
+    // Position in dom
+    _canvas.style.top = '50%';
+    _canvas.style.left = '50%';
+    _canvas.style.position = 'absolute';
+    _canvas.style.marginLeft = (0 - _canvas.clientWidth / 2).toString() + "px";
+    _canvas.style.marginTop = (0 - _canvas.clientHeight / 2).toString() + "px";
+
+
+  }
+
+  // TEST LINE FUNCTION IMPLEMENT THIS SOMEWHERE ELSE LOL; Also this was originall JS
+  // http://www.codeproject.com/Articles/16564/Drawing-lines-in-Mozilla-based-browsers-and-the-In
+  void drawEdge(_Edge edge) {
+
+    var x1 = edge.v1.x,
+        y1 = edge.v1.y,
+        x2 = edge.v2.x,
+        y2 = edge.v2.y;
+
+    var dx = x2 - x1;
+    var sx = 1;
+    var dy = y2 - y1;
+    var sy = 1;
+
+    if (dx < 0) {
+      sx = -1;
+      dx = -dx;
+    }
+    if (dy < 0) {
+      sy = -1;
+      dy = -dy;
+    }
+
+    dx = dx << 1;
+    dy = dy << 1;
+    _terminal.writeAt(x1, y1, "*");
+    if (dy < dx) {
+      var fraction = dy - (dx >> 1);
+      while (x1 != x2) {
+        if (fraction >= 0) {
+          y1 += sy;
+          fraction -= dx;
+        }
+        fraction += dy;
+        x1 += sx;
+        _terminal.writeAt(x1, y1, "*");
+      }
+    } else {
+      var fraction = dx - (dy >> 1);
+      while (y1 != y2) {
+        if (fraction >= 0) {
+          x1 += sx;
+          fraction -= dy;
+        }
+        fraction += dx;
+        y1 += sy;
+        _terminal.writeAt(x1, y1, "*");
+      }
+    }
+  }
+
+  void drawLine(x1, y1, x2, y2) {
+    drawEdge(new _Edge(new Point(x1, y1), new Point(x2, y2)));
+  }
+
+  void drawPolygon(List<Point> points, [bool fill]) {
+    assert(points.length >= 3);
+
+    _Polygon poly = new _Polygon.fromPoints(points);
+    for (var i = 0; i < poly.edges.length; i++) {
+      drawEdge(poly.edges[i]);
+    }
+
+    if (fill != null && fill) {
+
+      List<_Edge> sortedEdges = [];
+      sortedEdges = poly.edges;
+
+    }
+
+    return;
+  }
+
+  void drawRect(x1, y1, x2, y2, x3, y3, x4, y4) {
+    drawEdge(new _Edge(new Point(x1, y1), new Point(x2, y2)));
+    drawEdge(new _Edge(new Point(x2, y2), new Point(x3, y3)));
+    drawEdge(new _Edge(new Point(x3, y3), new Point(x4, y4)));
+    drawEdge(new _Edge(new Point(x4, y4), new Point(x1, y1)));
+  }
+
+}
+
+class _Edge implements Comparable {
+  Point _v1, _v2;
+
+  int _minY, _minYX, _maxY;
+  double _slope;
+
+
+  Point get v1 => this._v1;
+  Point get v2 => this._v2;
+  int get minY => this._minY;
+  int get minYX => this._minYX;
+  int get maxY => this._maxY;
+  double get slope => this._slope;
+
+  _Edge(Point v1, Point v2)
+      : this._v1 = v1,
+        this._v2 = v2 {
+
+    if (this._v1.y < this._v2.y) {
+      this._minY = _v1.y;
+      this._minYX = _v1.x;
+      this._maxY = _v2.y;
+    } else {
+      this._minY = _v2.y;
+      this._minYX = _v2.x;
+      this._maxY = _v1.y;
+    }
+
+    this._slope = (this._v1.y - this.v2.y) / (this._v1.x - this.v2.x);
+
+  }
+
+  int compareTo(_Edge) {
+    return 1;
+  }
+
+}
+
+class _Polygon {
+
+  List<_Edge> _edges = [];
+
+  List<_Edge> get edges => this._edges;
+
+  // Constructors
+  _Polygon.fromEdges(List<_Edge> edges) : this._edges = edges;
+
+  _Polygon.fromPoints(List<Point> points) {
+    for (var i = 0; i < points.length; i++) {
+      Point v1 = points[i];
+      Point v2 = (i < points.length - 1) ? points[i + 1] : points[0];
+      _edges.add(new _Edge(v1, v2));
+    }
+  }
+
+}
---
+++ web/main.dart
@@ -0,0 +1,141 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+
+import 'package:malison/malison.dart';
+import 'package:piecemeal/piecemeal.dart';
+import 'dart:async';
+
+import 'gfx.dart';
+
+void main() {
+  // Create or query a <canvas> element to bind it to.
+//  var canvas = new CanvasElement();
+//  document.body.children.add(canvas);
+  
+  var width = 120,
+      height = 50;
+  
+  var g = new Gfx(width, height);
+
+  // Create a new terminal. CanvasTerminal uses your browser's fonts.
+  // RetroTerminal uses a built in DOS-style Code Page 437 font.
+//  var terminal = new RetroTerminal.shortDos(width, height, canvas);
+
+  // You can draw strings at given positions.
+//  terminal.writeAt(0, 0, "This is a terminal!");
+
+//  for (var y = 0; y < height; y++) {
+//    for (var x = 0; x < width; x++) {
+//      if (y == 0 || y == height - 1) {
+//        terminal.writeAt(x, y, "*", Color.DARK_GOLD, Color.DARK_PURPLE);
+//      } else if (x == 0 || x == width - 1) {
+//        terminal.writeAt(x, y, "*", Color.DARK_GOLD, Color.DARK_PURPLE);
+//      }
+//    }
+//  }
+  // You can control the foreground and background color.
+  //terminal.writeAt(0, 1, "This is blue on green", Color.BLUE, Color.GREEN);
+
+
+  //g.drawLine(0, 0, 100, 30);
+  g.drawRect(0, 0, width-1, 0, width-1, height-1, 0, height-1);
+//  g.drawRect(0, height - 1 - (height~/4), width-1, height - 1 - (height~/4), width-1, height-1, 0, height-1);
+  
+
+  int farWallWidth  = 45;
+  int farWallHeight = 20;
+  
+  
+  // Front Wall
+  Point frontTopLeft     = new Point(((width-1)-farWallWidth)~/2, ((height-2)-farWallHeight)~/2);
+  Point frontTopRight    = new Point(frontTopLeft.x + farWallWidth, frontTopLeft.y);
+  Point frontBottomRight = new Point(frontTopRight.x, frontTopLeft.y + farWallHeight);
+  Point frontBottomLeft  = new Point(frontTopLeft.x, frontBottomRight.y);
+  
+  g.drawPolygon([frontTopLeft, frontTopRight, frontBottomRight, frontBottomLeft]);
+  
+  
+  // Left Wall
+  Point leftTopLeft = new Point(0,0);
+  Point leftTopRight = frontTopLeft;
+  Point leftBottomRight = frontBottomLeft;
+  Point leftBottomLeft = new Point(0, height-1);
+  
+  g.drawPolygon([leftTopLeft, leftTopRight, leftBottomRight, leftBottomLeft], true);
+  
+  // Top Wall
+  
+  Point topTopLeft = leftTopLeft;
+  Point topTopRight = new Point(width - 1, 0);
+  Point topBottomRight = frontTopRight;
+  Point topBottomLeft = frontTopLeft;
+  
+  // Right Wall 
+  Point rightTopLeft = topBottomRight;
+  Point rightTopRight = topTopRight;
+  Point rightBottomRight = new Point(width - 1, height - 1);
+  Point rightBottomLeft = frontBottomRight;
+  
+  g.drawPolygon([rightTopLeft, rightTopRight, rightBottomRight, rightBottomLeft]);
+  
+  g.drawPolygon([topTopLeft, topTopRight, topBottomRight, topBottomLeft]);
+  
+  
+  // Far door
+  var doorWidth = farWallWidth~/3.5;
+  var doorHeight = farWallHeight~/1.8;
+  
+  Point frontDoorTopLeft = new Point((farWallWidth - doorWidth)~/2 + frontTopLeft.x, frontBottomLeft.y - doorHeight);
+  Point frontDoorTopRight = new Point(frontDoorTopLeft.x + doorWidth, frontDoorTopLeft.y);
+  Point frontDoorBottomRight = new Point(frontDoorTopRight.x, frontBottomRight.y);
+  Point frontDoorBottomLeft = new Point(frontDoorTopLeft.x, frontDoorBottomRight.y);
+  
+  g.drawPolygon([frontDoorTopLeft, frontDoorTopRight, frontDoorBottomRight, frontDoorBottomLeft]);
+  
+  //  g.terminal.writeAt(0, 37, "LIFE: ", Color.WHITE);
+//  g.terminal.drawGlyph(6, 37, new Glyph.fromCharCode(CharCode.BLACK_HEART_SUIT, Color.RED));
+//  g.terminal.drawGlyph(7, 37, new Glyph.fromCharCode(CharCode.BLACK_HEART_SUIT, Color.RED));
+//  g.terminal.drawGlyph(8, 37, new Glyph.fromCharCode(CharCode.BLACK_HEART_SUIT, Color.RED));
+
+  // You can also draw individual glyphs -- character+color units.
+//  terminal.drawGlyph(3, 4, new Glyph.fromCharCode(CharCode.BLACK_HEART_SUIT, Color.RED, Color.BLACK));
+//
+//  terminal.drawGlyph(10, 10, new Glyph.fromCharCode(CharCode.PI, Color.AQUA));
+  
+//  var testString = "Hello... Welcome to Demon's Dwelling...";
+//  void slowText(x, y, i) {
+//    if(i!=0) {
+//      x = x+1;
+//    }
+//    
+//    terminal.writeAt(x, y, testString.substring(i, i+1));
+//    terminal.render();
+// 
+//    if(i < testString.length -1){
+//      new Timer(const Duration(milliseconds: 100), () => slowText(x,y,i+1));
+//    }
+//  }
+//  
+//  slowText(3, 7, 0);
+  
+  // Use the rng
+//  var r = new Rng(23).vecInRect(new Rect(0, 10, 10, 10));
+//  terminal.writeAt(r.x, r.y, "X");
+
+  // When you're done drawing, tell it to render all of the changes. It renders
+  // in batches for performance.
+//  terminal.render();
+
+//  canvas.style.margin = "0 auto";
+//  canvas.style.top = '50%';
+//  canvas.style.left = '50%';
+//  canvas.style.position = 'absolute';
+//  canvas.style.marginLeft = (0 - canvas.clientWidth / 2).toString() + "px";
+//  canvas.style.marginTop = (0 - canvas.clientHeight / 2).toString() + "px";
+
+  
+  
+
+}
